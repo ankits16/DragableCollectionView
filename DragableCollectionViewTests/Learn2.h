7a802e96c8919790361a6065745e0cb5685013d9
Ankit Sachan <ankit.sachan@globallogic.com>
2015-04-10 11:37:36 +0530

    Adding files Learn2.h

---
+++ DragableCollectionViewTests/Learn2.h
@@ -0,0 +1,13 @@
+//
+//  Learn2.h
+//  DragableCollectionView
+//
+//  Created by Ankit Sachan on 4/10/15.
+//  Copyright (c) 2015 Ankit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Learn2 : UIView
+
+@end
---
+++ DragableCollectionViewTests/Learn2.m
@@ -0,0 +1,21 @@
+//
+//  Learn2.m
+//  DragableCollectionView
+//
+//  Created by Ankit Sachan on 4/10/15.
+//  Copyright (c) 2015 Ankit. All rights reserved.
+//
+
+#import "Learn2.h"
+
+@implementation Learn2
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
